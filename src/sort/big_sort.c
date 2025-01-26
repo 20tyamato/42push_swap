625fe13ca4f5726f0171d1a8aaff9bc5afede4cf
[TYPrivate] <[tsukasa.soccer8@gmail.com]>
2025-01-26 16:15:02 +0900

    WIP

--- src/sort/big_sort.c
+++ src/sort/big_sort.c
@@ -5,7 +5,7 @@ void	rev_sort_b(t_stack *a, t_stack *b);
 
 void	sort_small_stack(t_stack *a, t_stack *b);
 
-struct s_operation_count
+typedef struct s_operation_count
 {
 	int	ra;
 	int	rb;
@@ -19,6 +19,57 @@ struct s_operation_count
 // a：ある数を一番上に持っていくには、何回回転すればいいかを計算する
 // b：ある数が入ったら、何回回転したら、ソートされるかを計算する
 
+int get_position(t_stack *stack, int value)
+{
+	t_list *current;
+	int i;
+
+	current = stack->top;
+	i = 0;
+	while (current)
+	{
+		if (current->value == value)
+			return (i);
+		current = current->next;
+		i++;
+	}
+	return (-1);
+}
+
+void	merge_operations(t_operation_count *operation_count);
+
+void	calc_min_operations(t_stack *a, t_stack *b)
+{
+	t_list	*current;
+	t_operation_count	*operation_count;
+	int min_operations;
+	int min_operations_number;
+
+	current = a->top;
+	operation_count = (t_operation_count *)malloc(sizeof(t_operation_count));
+	if (!operation_count)
+		return ;
+	// initが必要
+	min_operations = INT_MAX;
+	min_operations_number = current->value;
+	while (current)
+	{
+		if (get_position(a, current->value) < a->size / 2)
+			operation_count->ra = get_position(a, current->value);
+		else
+			operation_count->rra = get_position(a, current->value);
+		if (get_position(b, current->value) < b->size / 2)
+			operation_count->rb = get_position(b, current->value);
+		else
+			operation_count->rrb = get_position(b, current->value);
+		// merge results
+		merge_operations(operation_count);
+		// minの場合は、min_operations_numberを更新する
+		current = current->next;
+	}
+	// min_operations_numberを再度計算して、適用する
+}
+
 void	sort_big_stack(t_stack *a, t_stack *b)
 {
 	push_b(a, b);
@@ -29,6 +80,8 @@ void	sort_big_stack(t_stack *a, t_stack *b)
 	print_stack(b);
 
 	// aが3つになるまで、手順を計算した上でpbする
+	// aの要素一個一個に対して計算する
+	calc_min_operations(a, b);
 
 	// aが3つになったら、sort_small_stack(a, b)を実行する
 	if (a->size == 3)
