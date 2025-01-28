3acebf2682b22fa44e1d355a0d1765081db3ef11
[TYPrivate] <[tsukasa.soccer8@gmail.com]>
2025-01-28 14:24:39 +0900

    FASTER

--- src/sort/big/operation_utils.c
+++ src/sort/big/operation_utils.c
@@ -29,43 +29,76 @@ void	reset_operation_count(t_operation_count *operation_count)
 	operation_count->rrr = 0;
 }
 
-int	merge_operations(t_operation_count *operation_count)
+int ft_min(int a, int b)
 {
-	int	r_sum;
-	int	rr_sum;
+	return (a < b ? a : b);
+}
 
-	r_sum = 0;
-	rr_sum = 0;
-	while (operation_count->ra > 0
-		&& operation_count->rb > 0)
-	{
-		operation_count->rr++;
-		operation_count->ra--;
-		operation_count->rb--;
-	}
-	while (operation_count->rra > 0
-		&& operation_count->rrb > 0)
-	{
-		operation_count->rrr++;
-		operation_count->rra--;
-		operation_count->rrb--;
-	}
-	r_sum = operation_count->ra + operation_count->rb + operation_count->rr;
-	rr_sum = operation_count->rra + operation_count->rrb + operation_count->rrr;
-	if (r_sum < rr_sum)
-	{
-		operation_count->rra = 0;
-		operation_count->rrb = 0;
-		operation_count->rrr = 0;
-		return (r_sum);
-	}
-	else
-	{
-		operation_count->ra = 0;
-		operation_count->rb = 0;
-		operation_count->rr = 0;
-		return (rr_sum);
-	}
+int ft_max(int a, int b)
+{
+	return (a > b ? a : b);
+}
+
+int merge_operations(t_operation_count *op)
+{
+    int ra  = op->ra;
+    int rb  = op->rb;
+    int rra = op->rra;
+    int rrb = op->rrb;
+
+    int cost_rr      = ft_max(ra, rb);
+    int cost_rrr     = ft_max(rra, rrb);
+    int cost_ra_rrb  = ra + rrb;
+    int cost_rra_rb  = rra + rb;
+
+    int best_cost = cost_rr;
+    char best_scenario = 'A';
+
+    if (cost_rrr < best_cost)
+    {
+        best_cost = cost_rrr;
+        best_scenario = 'B';
+    }
+    if (cost_ra_rrb < best_cost)
+    {
+        best_cost = cost_ra_rrb;
+        best_scenario = 'C';
+    }
+    if (cost_rra_rb < best_cost)
+    {
+        best_cost = cost_rra_rb;
+        best_scenario = 'D';
+    }
+    op->rr  = 0;
+    op->rrr = 0;
+    op->ra  = 0;
+    op->rb  = 0;
+    op->rra = 0;
+    op->rrb = 0;
+
+    if (best_scenario == 'A')
+    {
+        op->rr = ft_min(ra, rb);
+        op->ra = ra - op->rr;
+        op->rb = rb - op->rr;
+    }
+    else if (best_scenario == 'B')
+    {
+        op->rrr = ft_min(rra, rrb);
+        op->rra = rra - op->rrr;
+        op->rrb = rrb - op->rrr;
+    }
+    else if (best_scenario == 'C')
+    {
+        op->ra  = ra;
+        op->rrb = rrb;
+    }
+    else // best_scenario == 'D'
+    {
+        op->rra = rra;
+        op->rb  = rb;
+    }
+    return best_cost;
 }
 
 void	repeat_operation(void (*op)(t_stack *, t_stack *),
