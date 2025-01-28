9e042dbb9b951434052bf6380c0bce3b4f396a6b
[TYPrivate] <[tsukasa.soccer8@gmail.com]>
2025-01-28 21:08:55 +0900

    NORM

--- src/stack/print_stack_side_by_side.c
+++ src/stack/print_stack_side_by_side.c
@@ -17,6 +17,26 @@ static void	print_stack_side_by_side_footer(void)
 	ft_putstr_fd("=======================================\n", STDOUT_FILENO);
 }
 
+static void	print_side_by_side_row(t_list **current1, t_list **current2)
+{
+	if (*current1)
+	{
+		print_aligned_int((*current1)->value, 18, 1);
+		*current1 = (*current1)->next;
+	}
+	else
+		print_aligned_str("", 18, 1);
+	ft_putstr_fd(" | ", STDOUT_FILENO);
+	if (*current2)
+	{
+		print_aligned_int((*current2)->value, 18, 0);
+		*current2 = (*current2)->next;
+	}
+	else
+		print_aligned_str("", 18, 0);
+	ft_putstr_fd("\n", STDOUT_FILENO);
+}
+
 void	print_stack_side_by_side(t_stack *stack1, t_stack *stack2)
 {
 	t_list	*current1;
@@ -26,23 +46,6 @@ void	print_stack_side_by_side(t_stack *stack1, t_stack *stack2)
 	current2 = stack2->top;
 	print_stack_side_by_side_header();
 	while (current1 || current2)
-	{
-		if (current1)
-		{
-			print_aligned_int(current1->value, 18, 1);
-			current1 = current1->next;
-		}
-		else
-			print_aligned_str("", 18, 1);
-		ft_putstr_fd(" | ", STDOUT_FILENO);
-		if (current2)
-		{
-			print_aligned_int(current2->value, 18, 0);
-			current2 = current2->next;
-		}
-		else
-			print_aligned_str("", 18, 0);
-		ft_putstr_fd("\n", STDOUT_FILENO);
-	}
+		print_side_by_side_row(&current1, &current2);
 	print_stack_side_by_side_footer();
 }
