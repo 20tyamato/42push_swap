57cbaea4c165870eb5cc1c5faf29a122819e5158
[TYPrivate] <[tsukasa.soccer8@gmail.com]>
2025-01-28 17:39:47 +0900

    NORM

--- src/sort/big/merge_operations.c
+++ src/sort/big/merge_operations.c
@@ -10,7 +10,8 @@ static void	update_best_cost_and_scenario(int *best_cost, char *best_scenario,
 	}
 }
 
-static int	find_best_scenario(t_operation_count *current_operation_count, char *best_scenario)
+static int	find_best_scenario(t_operation_count *current_op_count,
+				char *best_scenario)
 {
 	int	cost_rr;
 	int	cost_rrr;
@@ -18,10 +19,10 @@ static int	find_best_scenario(t_operation_count *current_operation_count, char *
 	int	cost_rra_rb;
 	int	best_cost;
 
-	cost_rr = ft_max(current_operation_count->ra, current_operation_count->rb);
-	cost_rrr = ft_max(current_operation_count->rra, current_operation_count->rrb);
-	cost_ra_rrb = current_operation_count->ra + current_operation_count->rrb;
-	cost_rra_rb = current_operation_count->rra + current_operation_count->rb;
+	cost_rr = ft_max(current_op_count->ra, current_op_count->rb);
+	cost_rrr = ft_max(current_op_count->rra, current_op_count->rrb);
+	cost_ra_rrb = current_op_count->ra + current_op_count->rrb;
+	cost_rra_rb = current_op_count->rra + current_op_count->rb;
 	best_cost = INT_MAX;
 	*best_scenario = '\0';
 	update_best_cost_and_scenario(&best_cost, best_scenario, cost_rr, 'A');
@@ -33,45 +34,47 @@ static int	find_best_scenario(t_operation_count *current_operation_count, char *
 
 static void	apply_scenario(t_operation_count *operation_count,
 						char best_scenario,
-						t_operation_count *current_operation_count)
+						t_operation_count *current_op_count)
 {
 	if (best_scenario == 'A')
 	{
-		operation_count->rr = ft_min(current_operation_count->ra, current_operation_count->rb);
-		operation_count->ra = current_operation_count->ra - operation_count->rr;
-		operation_count->rb = current_operation_count->rb - operation_count->rr;
+		operation_count->rr = ft_min(current_op_count->ra,
+				current_op_count->rb);
+		operation_count->ra = current_op_count->ra - operation_count->rr;
+		operation_count->rb = current_op_count->rb - operation_count->rr;
 	}
 	else if (best_scenario == 'B')
 	{
-		operation_count->rrr = ft_min(current_operation_count->rra, current_operation_count->rrb);
-		operation_count->rra = current_operation_count->rra - operation_count->rrr;
-		operation_count->rrb = current_operation_count->rrb - operation_count->rrr;
+		operation_count->rrr = ft_min(current_op_count->rra,
+				current_op_count->rrb);
+		operation_count->rra = current_op_count->rra - operation_count->rrr;
+		operation_count->rrb = current_op_count->rrb - operation_count->rrr;
 	}
 	else if (best_scenario == 'C')
 	{
-		operation_count->ra = current_operation_count->ra;
-		operation_count->rrb = current_operation_count->rrb;
+		operation_count->ra = current_op_count->ra;
+		operation_count->rrb = current_op_count->rrb;
 	}
 	else
 	{
-		operation_count->rra = current_operation_count->rra;
-		operation_count->rb = current_operation_count->rb;
+		operation_count->rra = current_op_count->rra;
+		operation_count->rb = current_op_count->rb;
 	}
 }
 
 int	merge_operations(t_operation_count *operation_count)
 {
-	t_operation_count	*current_operation_count;
+	t_operation_count	*current_op_count;
 	char				best_scenario;
 	int					best_cost;
 
-	current_operation_count = init_operation_count();
-	current_operation_count->ra = operation_count->ra;
-	current_operation_count->rb = operation_count->rb;
-	current_operation_count->rra = operation_count->rra;
-	current_operation_count->rrb = operation_count->rrb;
-	best_cost = find_best_scenario(current_operation_count, &best_scenario);
+	current_op_count = init_operation_count();
+	current_op_count->ra = operation_count->ra;
+	current_op_count->rb = operation_count->rb;
+	current_op_count->rra = operation_count->rra;
+	current_op_count->rrb = operation_count->rrb;
+	best_cost = find_best_scenario(current_op_count, &best_scenario);
 	reset_operation_counts(operation_count);
-	apply_scenario(operation_count, best_scenario, current_operation_count);
+	apply_scenario(operation_count, best_scenario, current_op_count);
 	return (best_cost);
 }
