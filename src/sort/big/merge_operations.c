dfd10c176c1092d7eb8f0a61a1e6828e44093898
[TYPrivate] <[tsukasa.soccer8@gmail.com]>
2025-01-28 17:32:55 +0900

    NORM

--- src/sort/big/merge_operations.c
+++ src/sort/big/merge_operations.c
@@ -2,37 +2,42 @@
 
 void	reset_operation_counts(t_operation_count *operation_count);
 
-static int find_best_scenario(int ra, int rb, int rra, int rrb, char *best_scenario)
+void	update_best_cost_and_scenario(int *best_cost, char *best_scenario,
+			int current_cost, char current_scenario)
 {
-	int cost_rr      = ft_max(ra, rb);
-	int cost_rrr     = ft_max(rra, rrb);
-	int cost_ra_rrb  = ra + rrb;
-	int cost_rra_rb  = rra + rb;
+	if (current_cost < *best_cost)
+	{
+		*best_cost = current_cost;
+		*best_scenario = current_scenario;
+	}
+}
 
-	int best_cost = cost_rr;
-	*best_scenario = 'A';
-	if (cost_rrr < best_cost)
-	{
-		best_cost = cost_rrr;
-		*best_scenario = 'B';
-	}
-	if (cost_ra_rrb < best_cost)
-	{
-		best_cost = cost_ra_rrb;
-		*best_scenario = 'C';
-	}
-	if (cost_rra_rb < best_cost)
-	{
-		best_cost = cost_rra_rb;
-		*best_scenario = 'D';
-	}
-	return best_cost;
+static int	find_best_scenario(int ra, int rb,
+				int rra, int rrb, char *best_scenario)
+{
+	int	cost_rr;
+	int	cost_rrr;
+	int	cost_ra_rrb;
+	int	cost_rra_rb;
+	int	best_cost;
+
+	cost_rr = ft_max(ra, rb);
+	cost_rrr = ft_max(rra, rrb);
+	cost_ra_rrb = ra + rrb;
+	cost_rra_rb = rra + rb;
+	best_cost = INT_MAX;
+	*best_scenario = '\0';
+	update_best_cost_and_scenario(&best_cost, best_scenario, cost_rr, 'A');
+	update_best_cost_and_scenario(&best_cost, best_scenario, cost_rrr, 'B');
+	update_best_cost_and_scenario(&best_cost, best_scenario, cost_ra_rrb, 'C');
+	update_best_cost_and_scenario(&best_cost, best_scenario, cost_rra_rb, 'D');
+	return (best_cost);
 }
 
 static void	apply_scenario(t_operation_count *operation_count,
-                           char best_scenario,
-                           int ra, int rb,
-                           int rra, int rrb)
+						char best_scenario,
+						int ra, int rb,
+						int rra, int rrb)
 {
 	if (best_scenario == 'A')
 	{
@@ -48,26 +53,31 @@ static void	apply_scenario(t_operation_count *operation_count,
 	}
 	else if (best_scenario == 'C')
 	{
-		operation_count->ra  = ra;
+		operation_count->ra = ra;
 		operation_count->rrb = rrb;
 	}
 	else
 	{
 		operation_count->rra = rra;
-		operation_count->rb  = rb;
+		operation_count->rb = rb;
 	}
 }
 
 int	merge_operations(t_operation_count *operation_count)
 {
-	int  ra   = operation_count->ra;
-	int  rb   = operation_count->rb;
-	int  rra  = operation_count->rra;
-	int  rrb  = operation_count->rrb;
+	int		ra;
+	int		rb;
+	int		rra;
+	int		rrb;
+	char	best_scenario;
+	int		best_cost;
 
-	char best_scenario;
-	int  best_cost = find_best_scenario(ra, rb, rra, rrb, &best_scenario);
+	ra = operation_count->ra;
+	rb = operation_count->rb;
+	rra = operation_count->rra;
+	rrb = operation_count->rrb;
+	best_cost = find_best_scenario(ra, rb, rra, rrb, &best_scenario);
 	reset_operation_counts(operation_count);
 	apply_scenario(operation_count, best_scenario, ra, rb, rra, rrb);
-	return best_cost;
+	return (best_cost);
 }
